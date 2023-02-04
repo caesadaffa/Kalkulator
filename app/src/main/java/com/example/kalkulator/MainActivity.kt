b380c214a14fe182802827ea1d1fe1858052704b
caesadaffa <caesadaffa@gmail.com>
2023-02-04 07:58:00 +0700

    intial commit

---
+++ app/src/main/java/com/example/kalkulator/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kalkulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+public class
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
